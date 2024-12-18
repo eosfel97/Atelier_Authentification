133546f7a31af242ce73876b6b53ca1a3a109c54
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2024-12-18 14:16:41 +0000

    admin

--- atelier2/page_admin.php
+++ atelier2/page_admin.php
@@ -2,9 +2,11 @@
 // Démarrer la session
 session_start();
 
+$expectedToken = 'admin_' . $_COOKIE['authToken'];
+
 // Vérifier si l'utilisateur est bien en possession d'un cookie valide
 // Dans le cas contraire il sera redirigé vers la page d'accueil de connexion
-if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== $token) {
+if (!isset($_COOKIE['authToken']) || $_COOKIE['authToken'] !== $expectedToken ) {
     header('Location: index.php');
     exit();
 }
